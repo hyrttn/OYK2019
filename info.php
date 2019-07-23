583e9959e9b86a07d465926f3dac6283003a700c
HAYRETTİN KILIÇASLAN <hhayrettink@gmail.com>
2019-07-23 09:53:44 +0300

    ilk

---
+++ info.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+$a=5;
+$b=6;
+?>
+
+<?php //echo `ping google.com` ?>
+<?php //if(true) echo "doğru" else echo "string"; ?>
+
+<?php $a = 3;
+$b = $a; // $b, $a'ya bir gönderimdir
+print "$a\n"; // 3 basar
+print "$b\n"; // 3 basar ?>
+
+
+
+</body>
+</html>
